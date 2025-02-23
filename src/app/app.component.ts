176733358a25cabb65f59b47653c348d06ff312d
Adri-Casas <acasas@cide.net>
2025-02-23 18:52:44 +0100

    Feat: Add notification Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterMenuComponent } from './components/footer-menu/footer-menu.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 // "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=4200 --user-data-dir="C:\chrome-debug"
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterMenuComponent],
+  imports: [RouterOutlet, FooterMenuComponent, NotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/pages/expenses-page/expenses-page.component.ts
+++ src/app/pages/expenses-page/expenses-page.component.ts
@@ -1,17 +1,23 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import {
   ListAdderComponent,
   ListItem,
 } from '../../components/list-adder/list-adder.component';
+import { NumberFormatterPipe } from '../../pipes/number-formatter.pipe';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-expenses-page',
   standalone: true,
-  imports: [ListAdderComponent],
+  imports: [ListAdderComponent, NumberFormatterPipe],
   templateUrl: './expenses-page.component.html',
   styleUrl: './expenses-page.component.scss',
 })
 export class ExpensesPageComponent {
+  /* Injections */
+  private readonly _notificationService = inject(NotificationService);
+
+  /* Variables */
   defaultExpensesList = signal<ListItem[]>([
     {
       id: '1',
@@ -45,6 +51,10 @@ export class ExpensesPageComponent {
 
   onAddExpense(expense: ListItem): void {
     this.defaultExpensesList.update((prev) => [...prev, expense]);
+    this._notificationService.createNotification({
+      type: 'success',
+      message: '¡Nuevo gasto añadido!',
+    });
   }
 
   onEditExpense(expense: ListItem): void {
@@ -54,11 +64,19 @@ export class ExpensesPageComponent {
     this.defaultExpensesList.update((prev) =>
       prev.map((item, i) => (i === index ? expense : item))
     );
+    this._notificationService.createNotification({
+      type: 'success',
+      message: '¡Gasto editado correctamente!',
+    });
   }
 
   onDeleteExpense(expenseId: string): void {
     this.defaultExpensesList.update((prev) =>
       prev.filter((item) => item.id !== expenseId)
     );
+    this._notificationService.createNotification({
+      type: 'success',
+      message: '¡Gasto eliminado!',
+    });
   }
 }
