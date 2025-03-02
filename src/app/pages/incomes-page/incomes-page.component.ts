360fff88f478c0ab2b5efe549bd2e056781c808a
Adri-Casas <acasas@cide.net>
2025-03-02 18:12:10 +0100

    Feat: Add incomes styles and functionallity

--- src/app/pages/incomes-page/incomes-page.component.ts
+++ src/app/pages/incomes-page/incomes-page.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import {
   ListAdderComponent,
   ListItem,
 } from '../../components/list-adder/list-adder.component';
+import { NotificationService } from '../../services/notification.service';
+import { NumberFormatterPipe } from '../../pipes/number-formatter.pipe';
 
 @Component({
   selector: 'app-incomes-page',
   standalone: true,
-  imports: [ListAdderComponent],
+  imports: [ListAdderComponent, NumberFormatterPipe],
   templateUrl: './incomes-page.component.html',
   styleUrl: './incomes-page.component.scss',
 })
 export class IncomesPageComponent {
-  defaultIncomesList: ListItem[] = [
+  /* Injections */
+  private readonly _notificationService = inject(NotificationService);
+
+  defaultIncomesList = signal<ListItem[]>([
     {
       id: '1',
       showOptions: false,
@@ -31,5 +36,46 @@ export class IncomesPageComponent {
       name: 'Inversiones',
       value: 12.5,
     },
-  ];
+  ]);
+
+  totalIncomes = computed(() =>
+    this.defaultIncomesList().reduce((acc, item) => acc + item.value, 0)
+  );
+
+  constructor() {
+    effect(() =>
+      console.log('Incomes executed edited: ', this.defaultIncomesList())
+    );
+  }
+
+  onAddIncome(income: ListItem): void {
+    this.defaultIncomesList.update((prev) => [...prev, income]);
+    this._notificationService.createNotification({
+      type: 'success',
+      message: '¡Nuevo ingreso añadido!',
+    });
+  }
+
+  onEditIncome(income: ListItem): void {
+    const index = this.defaultIncomesList().findIndex(
+      (item) => item.id === income.id
+    );
+    this.defaultIncomesList.update((prev) =>
+      prev.map((item, i) => (i === index ? income : item))
+    );
+    this._notificationService.createNotification({
+      type: 'success',
+      message: 'Ingreso editado correctamente!',
+    });
+  }
+
+  onDeleteIncome(incomeId: string): void {
+    this.defaultIncomesList.update((prev) =>
+      prev.filter((item) => item.id !== incomeId)
+    );
+    this._notificationService.createNotification({
+      type: 'success',
+      message: 'Ingreso eliminado!',
+    });
+  }
 }
