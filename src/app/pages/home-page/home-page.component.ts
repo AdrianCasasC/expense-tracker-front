8dac08c83fb58cd1fc4eb0fa350ac667d4483501
Adri-Casas <acasas@cide.net>
2025-03-22 19:32:14 +0100

    Feat: Add home last movements list

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { GraphComponent } from '../../components/graph/graph.component';
+import { ListItem } from '../../models/global.models';
+import { NumberFormatterPipe } from '../../pipes/number-formatter.pipe';
 
 @Component({
   selector: 'exptr-home-page',
   standalone: true,
-  imports: [GraphComponent],
+  imports: [GraphComponent, NumberFormatterPipe],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
@@ -22,4 +24,53 @@ export class HomePageComponent {
     { date: '2024-03-07', amount: 75 },
     { date: '2024-03-10', amount: 50 },
   ];
+
+  lastCosts = signal<ListItem[]>([
+    {
+      id: '1',
+      showOptions: false,
+      name: 'Sueldo',
+      value: 1800.0,
+      type: 'income',
+      category: 'salary',
+      date: new Date().toISOString(),
+    },
+    {
+      id: '2',
+      showOptions: false,
+      name: 'Intereses',
+      value: 50.0,
+      type: 'income',
+      category: 'interests',
+      date: new Date().toISOString(),
+    },
+    {
+      id: '3',
+      showOptions: false,
+      name: 'Comida',
+      value: 122.5,
+      type: 'expense',
+      category: 'food',
+      date: new Date().toISOString(),
+    },
+    {
+      id: '4',
+      showOptions: false,
+      name: 'Inversiones',
+      value: 12.5,
+      type: 'income',
+      category: 'inversions',
+      date: new Date().toISOString(),
+    },
+  ]);
+
+  onToggleItemOptions(itemId: string): void {
+    this.lastCosts().forEach((item) => {
+      if (item.id !== itemId) item.showOptions = false;
+    });
+    const selectedItem = this.lastCosts().find((item) => item.id === itemId);
+    if (selectedItem) {
+      selectedItem.showOptions = !selectedItem.showOptions;
+    }
+  }
 }
