5cbc87403a842da84c3f3baeab2cca431051e35b
Adri-Casas <acasas@cide.net>
2025-03-24 12:33:35 +0100

    Fix: Remove netlify functions

--- netlify/functions/server.ts
+++
@@ -1,5 +0,0 @@
-// netlify/functions/server.js
-const { createNodeRequestHandler } = require('@angular/ssr/node');
-const appServer = require('../../dist/expense-tracker-front/server/main'); // Asegúrate de la ruta correcta
-
-exports.handler = createNodeRequestHandler(appServer);
