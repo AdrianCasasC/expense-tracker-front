32840548ac93f6cb2ed3269e8cf09ceced643251
Adri-Casas <acasas@cide.net>
2025-04-02 18:55:25 +0200

    Feat: Update pro back url

--- src/environments/environment.pro.ts
+++ src/environments/environment.pro.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fresh-nerta-adrian-enterprise-6fabe499.koyeb.app',
+  apiUrl: 'https://expense-tracker-back-7mnh.onrender.com',
 };
